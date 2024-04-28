ce80097b35e871910d18c098cb77d22c18794fde
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-04-28 00:19:12 -0300

    :sparkles: Feat: Swagger OK - Implementação completa

--- projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/dto/ProdutoAlteracaoDto.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/dto/ProdutoAlteracaoDto.java
@@ -1,24 +1,40 @@
 package sptech.school.projetovolt.entity.produto.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(name = "Produto Alteracao DTO", description = "DTO para alteração de um produto")
 public class ProdutoAlteracaoDto {
     @Size(min = 3, max = 80)
+    @Schema(description = "Nome do produto", example = "Iphone 12 Pro Max")
     private String nome;
+
     @Size(max = 300)
+    @Schema(description = "Descrição do produto", example = "O Iphone 12 Pro Max é o mais novo lançamento da Apple")
     private String descricao;
+
     @Size(max = 100)
+    @Schema(description = "Categoria do produto", example = "Eletrônicos")
     private String categoria;
+
     @PositiveOrZero
+    @Schema(description = "Preço do produto", example = "10000.00")
     private Double preco;
+
     @PositiveOrZero
+    @Schema(description = "Quantidade em estoque do produto", example = "10")
     private Integer qtdEstoque;
+
     @Size(max = 45)
+    @Schema(description = "Estado geral do produto", example = "Novo")
     private String estadoGeral;
+
     @PositiveOrZero
+    @Schema(description = "Desconto do produto (10 = 10%)", example = "10")
     private Integer desconto;
 }
--- projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/dto/ProdutoConsultaDTO.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/dto/ProdutoConsultaDTO.java
@@ -1,14 +1,30 @@
 package sptech.school.projetovolt.entity.produto.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(name = "Produto Consulta DTO", description = "DTO para consulta de um produto")
 public class ProdutoConsultaDTO {
+
+    @Schema(description = "Nome do produto")
     private String nome;
+
+    @Schema(description = "Descrição do produto")
     private String descricao;
+
+    @Schema(description = "Categoria do produto")
     private String categoria;
+
+    @Schema(description = "Preço do produto")
     private Double preco;
+
+    @Schema(description = "Quantidade em estoque do produto")
     private Integer qtdEstoque;
+
+    @Schema(description = "Estado geral do produto")
     private String estadoGeral;
+
+    @Schema(description = "Desconto do produto (Ex: 10 = 10%)")
     private Integer desconto;
 }
