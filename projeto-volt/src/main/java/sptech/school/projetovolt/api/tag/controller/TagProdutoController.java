9f1287adb1b6b2c165963fb6a609d0877b44854a
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-04-28 20:19:58 -0300

    termino de endpoints de tag

--- projeto-volt/src/main/java/sptech/school/projetovolt/api/tag/controller/TagProdutoController.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/api/tag/controller/TagProdutoController.java
@@ -10,6 +10,7 @@ import sptech.school.projetovolt.entity.tag.dto.TagProdutoCriacaoDto;
 import sptech.school.projetovolt.entity.tag.dto.TagProdutoMapper;
 import sptech.school.projetovolt.entity.tag.repository.TagProdutoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,11 +21,31 @@ public class TagProdutoController {
 
     @PostMapping
     public ResponseEntity<TagProdutoConsultaDto> criarTag(@RequestBody TagProdutoCriacaoDto tag){
-
             TagProduto tagSalva = tagProdutoRepository.save(TagProdutoMapper.toEntity(tag));
             return ResponseEntity.status(201).body(TagProdutoMapper.toDto(tagSalva));
     }
 
+    @GetMapping
+    public ResponseEntity<List<TagProdutoConsultaDto>> listarTags(){
+        List<TagProduto> tags = tagProdutoRepository.findAll();
+
+        if(tags.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(TagProdutoMapper.toDto(tags));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TagProdutoConsultaDto> buscaTag(@PathVariable int id){
+        Optional<TagProduto> tag = tagProdutoRepository.findById(id);
+
+        if(tag.isPresent()){
+            return ResponseEntity.ok(TagProdutoMapper.toDto(tag.get()));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
     @PatchMapping("/{id}")
     public ResponseEntity<TagProdutoConsultaDto> alterarTag(@PathVariable int id, @RequestParam @Valid String tag){
         Optional<TagProduto> tagParaAlterar = tagProdutoRepository.findById(id);
