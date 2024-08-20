0f904b631217d8394d40c3701c10426c4fe40708
Larissa Miranda Sonoda <sonoda.lari@gmail.com>
2024-08-20 19:33:29 -0300

    fix: pesquisa produtos

--- projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/repository/ProdutoRepository.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/entity/produto/repository/ProdutoRepository.java
@@ -16,8 +16,8 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
     ProdutoConsultaDTO findByNome(String nome);
     List<Produto> findAllByNome(String textoBusca);
 
-    @Query(value = "SELECT p.id as id, lower(p.nome) as nome, p.descricao,\n" +
-            "p.categoria, p.preco, p.qtd_estoque, p.estado_geral, p.desconto FROM tb_produto AS p WHERE p.nome LIKE LOWER(CONCAT('%', ?, '%'))", nativeQuery = true)
+    @Query(value = "SELECT p.id as id, fnRemoveAccents(lower(p.nome)) as nome, p.descricao,\n" +
+            "p.categoria, p.preco, p.qtd_estoque, p.estado_geral, p.desconto FROM tb_produto AS p WHERE p.nome COLLATE utf8mb4_unicode_ci LIKE LOWER(CONCAT('%', ?, '%'))", nativeQuery = true)
     List<Produto> findAllByNomeContainsIgnoreCase(String textoBusca);
 
     List<Produto> findByOrderByPrecoDesc();
