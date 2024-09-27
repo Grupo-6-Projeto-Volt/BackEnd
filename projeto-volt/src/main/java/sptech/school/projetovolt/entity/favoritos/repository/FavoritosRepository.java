6383b8224de1b77f99127dec49cc3b58f8d99b89
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-09-27 20:28:22 -0300

    ✨Feat: Endpoint para verificar se  prod está  favoritado

--- projeto-volt/src/main/java/sptech/school/projetovolt/entity/favoritos/repository/FavoritosRepository.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/entity/favoritos/repository/FavoritosRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface FavoritosRepository extends JpaRepository<Favoritos, Integer> {
     @Query(value = "SELECT * FROM tb_favoritos WHERE fk_usuario = :idUsuario", nativeQuery = true)
     List<Favoritos> findAllByFkUsuario(Integer idUsuario);
+
+    @Query(value = "SELECT * FROM tb_favoritos WHERE fk_usuario = :idUsuario AND fk_produto = :idProduto", nativeQuery = true)
+    Favoritos findByUsuarioIdAndProdutoId(int idUsuario, int idProduto);
 }
