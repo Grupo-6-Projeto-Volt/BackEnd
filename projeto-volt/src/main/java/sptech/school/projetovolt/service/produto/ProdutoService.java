65f6830a6a3124879eff43dcab65dae8de69100c
Davi Hilário <davihilario23@gmail.com>
2024-10-06 17:51:43 -0300

    fix: adicionando ID do produto encontrado ao novo produto atualizado

--- projeto-volt/src/main/java/sptech/school/projetovolt/service/produto/ProdutoService.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/service/produto/ProdutoService.java
@@ -52,6 +52,7 @@ public class ProdutoService {
         buscarProdutoPorId(id);
         Categoria categoria = categoriaService.buscarCategoriaPorId(idCategoria);
         produto.setCategoria(categoria);
+        produto.setId(id);
         return produtoRepository.save(produto);
     }
 
