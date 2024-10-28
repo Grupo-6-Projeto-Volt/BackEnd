b81642f1535603824ee7c617e6895aacdcbb99ed
Felipe Santos <125617308+FelipeSantos-cco@users.noreply.github.com>
2024-10-27 22:08:53 -0300

    Feature: endpoint de importar arquivo TXT (#60)
    Co-authored-by: resdom <125586831+CaetanoDomingues@users.noreply.github.com>

--- projeto-volt/src/main/java/sptech/school/projetovolt/api/produto/controller/ProdutoController.java
+++ projeto-volt/src/main/java/sptech/school/projetovolt/api/produto/controller/ProdutoController.java
@@ -7,9 +7,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import sptech.school.projetovolt.api.util.ResponseUtil;
 import sptech.school.projetovolt.entity.produto.Produto;
 import sptech.school.projetovolt.service.hashtable.HashTableService;
@@ -128,6 +130,7 @@ public class ProdutoController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
     @GetMapping(value = "/exportar-txt",produces = "text/txt")
     public ResponseEntity<byte[]> exportarArquivoTxt(){
         try{
@@ -135,6 +138,21 @@ public class ProdutoController {
         }catch (Exception  e){
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping(value = "/importar-txt", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Faz o upload de um arquivo TXT para importar produtos", method = "POST", description = "Processa o arquivo TXT e insere produtos no banco de dados")
+    public ResponseEntity<String> importarArquivo(@RequestParam("file") MultipartFile file) {
+
+        if (file.isEmpty()) return ResponseEntity.badRequest().body("Arquivo vazio!");
+
+        try {
+            produtoService.processarArquivoImportacao(file);
+            return ResponseEntity.ok("Arquivo importado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Erro ao importar o arquivo.");
         }
     }
 
